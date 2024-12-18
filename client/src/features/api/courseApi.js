db81a2a4e8e7fc073c91944b16d28177de017821
Ramesh Das <rameshdas@Rameshs-MacBook-Air.local>
2024-12-18 14:53:45 +0530

    Project is done

--- client/src/features/api/courseApi.js
+++ client/src/features/api/courseApi.js
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_API = "http://localhost:3000/api/v1/course";
+const COURSE_API = import.meta.env.MODE === "development"
+  ? "http://localhost:3000/api/v1/course"
+  : "/api/v1/course";
 
 export const courseApi = createApi({
   reducerPath: "courseApi",
--- client/src/features/api/courseProgressApi.js
+++ client/src/features/api/courseProgressApi.js
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PROGRESS_API = "http://localhost:3000/api/v1/progress";
+const COURSE_PROGRESS_API = import.meta.env.MODE === "development"
+  ? "http://localhost:3000/api/v1/progress"
+  : "/api/v1/progress";
 
 export const courseProgressApi = createApi({
   reducerPath: "courseProgressApi",
